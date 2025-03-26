94045c25e0d4b5dd24a911b3133fc1cd3d6c60fe
DeNiSsca911 <denwertyu@gmail.com>
2025-03-26 19:53:59 +0300

    Update_Mane.py

--- Mane.py
+++ Mane.py
@@ -2,9 +2,9 @@ class Warrior():
 
     def __init__(self,name,power,endurance,hair_color):
         self.name = name
-        self.power = power
-        self.endurance = endurance
-        self.hare_color = hair_color
+        self.power = int(power)
+        self.endurance = int(endurance)
+        self.hair_color = hair_color
 
     def sleep(self):
         print(f"{self.name} лег спать ")
@@ -16,13 +16,36 @@ class Warrior():
 
     def hit(self):
         print(f"{self.name} бьет кого то ")
-        self.endurance += 6
+        self.endurance -= 6
 
     def walk(self):
         print(f"{self.name} гуляет ")
 
     def info(self):
         print(f" имя воина {self.name}")
-        print(f" цвет волос воина {self.hare_color}")
+        print(f" цвет волос воина {self.hair_color}")
         print(f"сила воина {self.power}")
         print(f"выносливость воина {self.endurance} ")
+
+war1 = Warrior("Степа","76","54","коричневый")
+war2 = Warrior("Егор","55","76","блондин")
+
+war1.info()
+print(war1.endurance)
+war1.sleep()
+print(war1.endurance)
+
+print(war1.power)
+war1.eat()
+print(war1.power)
+
+war2.info()
+print(war2.power)
+war2.eat()
+print(war2.power)
+
+print(war2.endurance)
+war2.hit()
+print(war2.endurance)
+
+
